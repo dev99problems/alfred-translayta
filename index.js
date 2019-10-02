c289999d31e2d74153f3caf6ae39902067e384e1
Eugene Chulkov <genechulkov@gmail.com>
2019-10-02 10:55:19 +0300

    add translation target detection; move utils to separate file

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
 const alfy = require('alfy')
 const translate = require('@vitalets/google-translate-api')
-const _get = require('lodash.get')
+const { convertIntoItems, getOtherTranslations } = require('./utils')
 
-const convertIntoItems = (allTranslations = {}) =>
-  Object.keys(allTranslations).reduce((acc, partOfSpeech) => {
-    const translations = allTranslations[partOfSpeech]
-    const items = translations.map((item, idx) => ({
-      title: item,
-      subtitle: idx === 0 ? `${partOfSpeech} ⤵️` : ''
-    }))
-
-    return [...acc, ...items]
-  }, [])
-
-const getOtherTranslations = input => {
-  try {
-    const parsedResponse = JSON.parse(input)
-    const otherTranslations = _get(parsedResponse, '[1]') || []
-
-    const allTranslations = otherTranslations.reduce((acc, translation) => {
-      const partOfSpeech = _get(translation, '[0]')
-      const translationVariants = _get(translation, '[1]')
-
-      return {
-        ...acc,
-        [partOfSpeech]: translationVariants
-      }
-    }, {})
-
-    return allTranslations
-  } catch (err) {
-    console.error(err)
-    return {}
+const intl = {
+  bestTranslMsg: {
+    ru: 'найболее подходящий перевод',
+    en: 'best match translation'
+  },
+  errMsg: {
+    ru: 'При получении перевода произошла ошибка',
+    en: 'Error occured while getting translation'
   }
 }
 
-const translateQuery = process.argv[2] || ''
+const userInput = process.argv[2] || ''
 
-translate(translateQuery, {to: 'ru', raw: true}).then(response => {
+const isASCII = str => [].every.call(str, sign => sign.charCodeAt(0) <= 127)
+
+const getTranslationTarget = input => isASCII(input) ? 'ru' : 'en'
+const to = getTranslationTarget(userInput)
+
+translate(userInput, {to, raw: true}).then(response => {
   const otherTranslations = getOtherTranslations(response.raw)
 
   const mainTranslation = {
     title: response.text,
-    subtitle: 'best fit translation'
+    subtitle: intl.bestTranslMsg[to]
   }
 
   const allVariants = [mainTranslation].concat(
@@ -51,6 +34,6 @@ translate(translateQuery, {to: 'ru', raw: true}).then(response => {
 
   alfy.output(allVariants)
 }).catch(err => {
-  alfy.output([{ title: 'Error while getting translation'}])
+  alfy.output([{ title: intl.errMsg[to]}])
 })
 
---
+++ utils.js
@@ -0,0 +1,32 @@
+const _get = require('lodash.get')
+
+module.exports.convertIntoItems = (otherTranslations = {}) =>
+  Object.keys(otherTranslations).reduce((acc, partOfSpeech) => {
+    const translations = otherTranslations[partOfSpeech]
+    const items = translations.map((item, idx) => ({
+      title: item,
+      subtitle: idx === 0 ? `${partOfSpeech} ⤵️` : ''
+    }))
+
+    return [...acc, ...items]
+  }, [])
+
+module.exports.getOtherTranslations = input => {
+  try {
+    const parsedResponse = JSON.parse(input)
+    const otherTranslations = _get(parsedResponse, '[1]') || []
+
+    return otherTranslations.reduce((acc, translation) => {
+      const partOfSpeech = _get(translation, '[0]')
+      const translationVariants = _get(translation, '[1]')
+
+      return {
+        ...acc,
+        [partOfSpeech]: translationVariants
+      }
+    }, {})
+  } catch (err) {
+    console.error(err)
+    return {}
+  }
+}
