4ed31c6e8b89917907579dfb6ef39fb46aceef0e
Eugene Chulkov <genechulkov@gmail.com>
2019-10-31 22:55:18 +0200

    add saving favorites to file on a disk

--- src/commands/add-to-favorites.js
+++ src/commands/add-to-favorites.js
@@ -1,15 +1,35 @@
+const fs = require('fs')
 const { favoritesCache } = require('../cache')
 
 const { word, translations } = process.env
 
-const { items } = favoritesCache.get() || {}
+const { items = [] } = favoritesCache.get() || {}
 
-favoritesCache.set({
-  items: [
-    {
-      title: word,
-      subtitle: translations
-    },
-    ...items
-  ]
+const updatedItems = [
+  {
+    title: word,
+    subtitle: translations
+  },
+  ...items
+]
+
+saveFavorites(updatedItems, () => {
+  favoritesCache.set({ items: updatedItems })
 })
+
+function saveFavorites(items, cb) {
+  const convertedData = items.reduce((acc, item) => {
+    acc[item.title] = item.subtitle
+    return acc
+  }, {})
+
+  const fileData = JSON.stringify(convertedData)
+
+  fs.writeFile('./favorites.txt', fileData, err => {
+    if (err) throw err
+
+    if (typeof cb === 'function') {
+      cb()
+    }
+  })
+}
