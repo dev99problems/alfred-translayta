c934df17eabcbd7e07c904dc523852f0b4cea55c
Eugene Chulkov <genechulkov@gmail.com>
2019-11-03 16:03:26 +0200

    add visual separator for favorites list

--- src/cache/favorites-cache.js
+++ src/cache/favorites-cache.js
@@ -21,10 +21,27 @@ class FavoritesCache extends Cache {
 
   getFavorites() {
     const items = this.items
+    const favoritesList = []
 
-    return Object.keys(items)
-      .map(key => ({ title: key, subtitle: items[key] }))
-      .reverse()
+    const keys = Object.keys(items).reverse()
+    const keysCount = keys.length
+
+    for (let i = 0; i < keysCount; i++) {
+      const blockSize = 7
+      if (i && !(i % blockSize)) {
+        let lastIndex = i + blockSize
+        if (lastIndex > keysCount) {
+          lastIndex = keysCount
+        }
+        const subtitle = `${i + 1}—${lastIndex} ⤵️`
+        favoritesList.push({ title: ' ', subtitle, icon: { path: ' ' } })
+      }
+
+      const key = keys[i]
+      favoritesList.push({ title: key, subtitle: items[key] })
+    }
+
+    return favoritesList
   }
 }
 
