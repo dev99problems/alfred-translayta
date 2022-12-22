41cd926e4d52d8b9de85f026f7030a4cdc48d9be
Eugene Chulkov <genechulkov@gmail.com>
2022-12-22 20:10:18 +0000

    refactor: remove addToFavoritesAction from output.js

--- src/__tests__/output.test.js
+++ src/__tests__/output.test.js
@@ -3,8 +3,7 @@ const {
   formatMainTranslation,
   formatOtherTranslations,
   formatAutoCorrection,
-  formatLastSearch,
-  addToFavoritesAction
+  formatLastSearch
 } = require('../output')
 
 const getStrLanguage = getTranslationSource
@@ -189,49 +188,5 @@ describe('output', () => {
       expect(hintLang).toBe(targetLang)
       expect(typeof lastSearchItem.icon.path).toBe('string')
     })
-  })
-
-  describe('addToFavoritesAction', () => {
-    test('prepares addToFavs action to alfy-format, with proper arguments', () => {
-      const userInput = 'castle'
-      const translation = 'замок'
-      const otherTranslations = [
-        'замок',
-        'контейнер',
-        'сховище',
-        'палац',
-        'тура',
-        'фортеця'
-      ]
-      const targetLang = 'uk'
-
-      const addToFavsItem = addToFavoritesAction(
-        userInput,
-        translation,
-        otherTranslations,
-        targetLang
-      )
-      const titleLang = getStrLanguage(addToFavsItem.title)
-      const hintLang = getStrLanguage(addToFavsItem.subtitle)
-      const otherTranslationsInArg = /"translations":"(.*)"/gi.exec(
-        addToFavsItem.arg
-      )[1]
-      console.log('otherTranslationsInArg', otherTranslationsInArg)
-
-      expect(addToFavsItem).toEqual({
-        title: '/додати в обране',
-        subtitle: 'переглянути обране можна натиснувши "."',
-        icon: { path: './icons/bookmark.png' },
-        arg:
-          '{"alfredworkflow":{"variables":{"action":"add","word":"castle","translations":"замок, контейнер, сховище, палац, тура, фортеця"}}}'
-      })
-
-      expect(titleLang).toBe(targetLang)
-      expect(hintLang).toBe(targetLang)
-      expect(typeof addToFavsItem.icon.path).toBe('string')
-      expect(otherTranslations.join(', ')).toEqual(otherTranslationsInArg)
-    })
-
-    // TODO: add test cases for 1 otherTranslations and 0 otherTranslations
   })
 })
